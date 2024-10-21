a992f395447caeecd9c5d4dedb2b5fb96b81f71a
ZINOVEV <borda3xx@bk.ru>
2024-10-21 15:42:55 +0700

    Работа с типами данных

--- pr001.java
+++ pr001.java
@@ -1,5 +1,39 @@
 class pr001 {	
 	public static void main(String args[]) {
 		System.out.println("Первая Java программа");
+		//Преобразование типов
+
+		//Целые типы данных
+		byte b;//8 бит
+		b = -128;
+		b = 127;
+		b = (byte)256;
+		System.out.println(b);
+		short sh;//16 бит
+		sh = (short)65536;
+		int intg;//32 бит
+		long l;// 64 бит
+		intg = 2000000000;
+		l = intg;
+		intg = intg*intg*intg;
+		System.out.println(intg);
+		l = l*l*l;
+		System.out.println(l);
+		//Вецественные типы
+		float f;//32 бит
+		double dd;//64 бит
+		//Символьный тип данных
+		char ch;//16 бит
+		ch = 'А';
+		ch++; ++ch;
+		for (int i=0; i<68; i++)
+			System.out.print(ch++ + " ");
+		System.out.println();
+		//Логический тип
+		boolean bool;//
+		bool = true;
+		bool = false;
+		
+
 	}
 }
