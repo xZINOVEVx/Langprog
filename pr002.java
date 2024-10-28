7bdfb1dfca6a76d65afdc82de0bbba9c423403c4
ZINOVEV <borda3xx@bk.ru>
2024-10-28 13:33:03 +0700

    Добавили циклы for, while, do-while

--- pr002.java
+++ pr002.java
@@ -2,7 +2,7 @@ class pr002 {
 	public static void main(String args[])
        			throws java.io.IOException {
 		int intg = 10, intgr = 20;
-		char ch, answer = 'R';
+/*		char ch, answer = 'R';
 		System.out.println("Попробуй угадать симол");
 		ch = (char) System.in.read();
 		if (ch == answer)
@@ -14,6 +14,7 @@ class pr002 {
 			else
 				System.out.println("Ниже, чем указанная");
 		}
+*/	
 		boolean bool = true;
 		
 		//int i;
@@ -43,7 +44,40 @@ class pr002 {
 		else
 			System.out.println("bool Равен false");
 
-                           
+		System.out.println("\n");
+		//Оператор for
+		for(int i=0, j=10; i<j; i++, j--)
+			System.out.println("i и j: " + i + " " + j + "\n");
+
+
+		for(int i=0; i<10;) {
+			System.out.println("Проход №" + i);
+			i++;
+		}
+		//Бесконечный цикл
+		//for(;;);
+		int sum=0;
+		for(int i=1; i<=5; sum+= i++);
+		System.out.println("\nСумма равна: " + sum);
+
+		System.out.println("\n");
+		//Цикл while
+		char ch1='А';
+		while(ch1 <= 'я') {
+			System.out.print(ch1 + " ");
+			ch1++;	
+		}
+		System.out.println();
+		System.out.println(((int) 'е') + " " + ((int) 'Е') + " " + ((int) 'ё'));
+
+		System.out.println("\n");
+		//Цилк do while
+		char ch2;
+		do {
+			System.out.print("Введите символ и нажмите \"RETURN\": ");
+			ch2 = (char) System.in.read();
+		} while (ch2 != 'q');
 
 	}
+	
 }
