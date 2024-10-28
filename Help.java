481b2b8baa0ba0912802b55418ab98e5df687b2e
ZINOVEV <borda3xx@bk.ru>
2024-10-28 14:39:08 +0700

    2-ая версия класса Help - добавили операторы цикла

--- Help.java
+++ Help.java
@@ -1,13 +1,22 @@
 class Help {
 	public static void main(String[] args)
 		throws java.io.IOException {
-		char choice;
+		char choice, ignore;
+		do {
 
-		System.out.println("Справочная система по операторам:");
-		System.out.println(" 1. if");
-		System.out.println(" 2. switch");
-		System.out.print("Выберите номер раздела: ");
-		choice = (char) System.in.read();
+
+			System.out.println("Справочная система по операторам:");
+			System.out.println(" 1. if");
+			System.out.println(" 2. switch");
+			System.out.println(" 3. for");
+			System.out.println(" 4. while");
+			System.out.println(" 5. do-while");
+			System.out.print("Выберите номер раздела: ");
+			choice = (char) System.in.read();
+			do {
+				ignore = (char) System.in.read();
+			} while(ignore != '\n');
+		} while(choice < '1' |  choice > '5');
 
 		System.out.println("\n");
 		switch (choice) {
@@ -25,8 +34,26 @@ class Help {
 				System.out.println("  //...");
 				System.out.println("}");
 				break;
-			default:
-				System.out.println("Неправильно указан номер раздела");
+			case '3':
+				System.out.println("Оператор цикла \"for\":\n");
+				System.out.println("for (инициализация;условие; итерационные действия)");	
+				System.out.println("	оператор; или {блок операторов}");
+				break;
+							
+			case '4':
+				System.out.println("Оператор цикла \"while\":\n");
+				System.out.println("while(условие) операторы; или {блок операторов}");
+				break;
+			case '5':
+				System.out.println("Оператор цикла \"do-while\":\n");
+				System.out.println("do {");
+				System.out.println("	операторы");
+				System.out.println("} while (условие);");
+				break;
+
+			
+				
+
 		}
 	}
 }
