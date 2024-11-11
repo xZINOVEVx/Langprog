a1efc298ba8894b2cf9c8d218bc986c9448e3bf5
ZINOVEV <borda3xx@bk.ru>
2024-11-11 16:18:59 +0700

    Первая версия очереди

---
+++ Queue.java
@@ -0,0 +1,57 @@
+class Queue {
+	char[] q;
+	int putloc, getloc; //указатели на индекс для помещение и извлечение символов
+	Queue(int size) {
+		q = new char[size];//выделяем память под нужную очередь нужного размера
+		putloc=getloc=0;
+	}
+	//Метод для помещения символа в очередь
+	void put(char ch) {
+		if(putloc==q.length) {
+			System.out.println(" - очередь переполнена");
+			return;
+		}
+		q[putloc++] = ch;
+	}
+	//метод для извлечение символа из очереди
+	char get() {
+		if(getloc==putloc) {
+			System.out.println( " - очередь переполнена");
+			return (char) 0;
+		}
+		return q[getloc++];
+	}	
+}
+class QDemo {
+	public static void main(String[] args) {
+		Queue bigQ = new Queue(100);
+		Queue smallQ = new Queue(4);
+		char ch;
+		int i;
+
+		System.out.println("Сохраняем алфавит в очередь bigQ");
+		for(i = 0; i < 26; i++)
+			bigQ.put((char) ('A' + i));
+
+		System.out.println("Содержание очереди bigQ: ");
+		for(i = 0; i < 26; i++) {
+			ch = bigQ.get();
+			if(ch != (char) 0)
+				System.out.print(ch);
+		}
+		System.out.println("\n");
+		System.out.println("Сохраняем символы очереди smallQ");
+		for(i = 0; i < 5; i++) {
+			System.out.println("Попытка сохранения " + (char) ('Z' - i));
+			smallQ.put((char) ('Z' - i));
+			System.out.println();
+		}
+		System.out.println("Извлекаем символы из очереди smallQ");
+		for(i = 0;i < 5; i++) {
+			ch = smallQ.get();
+			if(ch !=(char)0)
+				System.out.println(ch);
+		}
+		System.out.println();
+	}
+}
